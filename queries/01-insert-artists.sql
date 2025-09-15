c32987140869bf8db41d697deb9d594c4303c145
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-09-15 09:55:56 -0300

    feat: Adiciona os primeiros valores

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -9,3 +9,7 @@ CREATE TABLE artistas (
     musica_mais_ouvida VARCHAR(50),
     ativo BOOLEAN DEFAULT TRUE,
 );
+
+INSERT INTO artistas (nome, tipo, membros, genero_musical, pais_origem, ano_inicio, musica_mais_ouvida, ativo)
+VALUES 
+('BTS', 'Banda', 7, 'K-pop', 'Coreia do Sul', 2013, 'Butter', TRUE);
