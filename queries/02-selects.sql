9e3e75ded835cdb7a8adcf03a577df06e484dd63
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-09-15 10:51:42 -0300

    feat: finaliza select

--- queries/02-selects.sql
+++ queries/02-selects.sql
@@ -1,3 +1,11 @@
 SELECT * FROM artistas;
 
 SELECT COUNT(*) AS total_artistas FROM artistas;
+
+SELECT nome, musica_mais_ouvida FROM artistas;
+
+SELECT nome, musica_mais_ouvida FROM artistas
+WHERE pais_origem = 'Reio Unido';
+
+SELECT nome, musica_mais_ouvida FROM artistas
+WHERE id = 3;
