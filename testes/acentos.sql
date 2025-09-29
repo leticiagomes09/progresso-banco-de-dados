02c6eb57d5d3112a876c86925936ee0ede2ec148
leticiagomes09 <leticia.m.gomes10@aluno.senai.br>
2025-09-29 11:40:11 -0300

    feat: Cria  pasta testes

---
+++ testes/acentos.sql
@@ -0,0 +1,12 @@
+CREATE TABLE teste (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+INSERT INTO teste (nome)
+VALUES 
+('Letícia'),
+('João'),
+('Júlia'),
+('Vinícius'),
+('Márcia');
